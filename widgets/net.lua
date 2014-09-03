d8b9e2edbf2519139b181f30644a2e6441ca173c
Olivier Scholder <o.scholder@gmail.com>
2014-09-03 21:06:56 +0200

    Fix net widget

--- widgets/net.lua
+++ widgets/net.lua
@@ -2,31 +2,41 @@ local vicious = require("vicious")
 local wibox = require("wibox")
 local beautiful = require("beautiful")
 local awful =  require("awful")
-
+local setmetatable = setmetatable
 module("widgets.net")
 
+local net = {}
+
 -- Net widget
+
+
+function net:swap_mode()
+	self.down.mode = 1 - self.down.mode
+	self.up.mode = 1 - self.up.mode
+	vicious.force ({ self.up, self.down })
+end
+
 function new()
-	local net = {}
+	local self = setmetatable({}, net)
 	vicious.cache(vicious.widgets.net)
-	net.up_icon = wibox.widget.imagebox()
-	net.up_icon:set_image(beautiful.widget_netup)
-	net.down_icon = wibox.widget.imagebox()
-	net.down_icon:set_image(beautiful.widget_netdown)
-	net.up = wibox.widget.textbox()
-	net.down = wibox.widget.textbox()
-	net.up.mode=0
-	net.down.mode=0
-	vicious.register(net.down, vicious.widgets.net, function (widget, args)
-		if net.down.mode == 0
+	self.up_icon = wibox.widget.imagebox()
+	self.up_icon:set_image(beautiful.widget_netup)
+	self.down_icon = wibox.widget.imagebox()
+	self.down_icon:set_image(beautiful.widget_netdown)
+	self.up = wibox.widget.textbox()
+	self.down = wibox.widget.textbox()
+	self.up.mode=0
+	self.down.mode=0
+	vicious.register(self.down, vicious.widgets.net, function(widget, args)
+		if self.down.mode == 0
 		then
 			return '<span color="#ff8800">' .. args['{eth0 down_kb}'] .. 'kb/s</span>'
 		else
 			return '<span color="#ffff00">' .. args['{eth0 rx_mb}'] .. 'Mb</span>'
 		end
 	end,5)
-	vicious.register(net.up, vicious.widgets.net, function (widget, args)
-		if net.up.mode == 0
+	vicious.register(self.up, vicious.widgets.net, function(widget, args)
+		if self.up.mode == 0
 		then
 			return '<span color="#55ff55">' .. args['{eth0 up_kb}'] .. 'kb/s</span>'
 		else
@@ -34,27 +44,27 @@ function new()
 		end
 	end,5)
 
+	self.up_icon:buttons (awful.button ({}, 1, function()
+		self.down.mode = 1 - self.down.mode
+		self.up.mode = 1 - self.up.mode
+		vicious.force ({ self.up, self.down })
+	end))
+	self.down_icon:buttons (awful.button ({}, 1,function()
+		self.down.mode = 1 - self.down.mode
+		self.up.mode = 1 - self.up.mode
+		vicious.force ({ self.up, self.down })
+	end))
+	self.up:buttons (awful.button ({}, 1, function()
+		self.down.mode = 1 - self.down.mode
+		self.up.mode = 1 - self.up.mode
+		vicious.force ({ self.up, self.down })
+	end))
+	self.down:buttons (awful.button ({}, 1, function()
+		self.down.mode = 1 - self.down.mode
+		self.up.mode = 1 - self.up.mode
+		vicious.force ({ self.up, self.down })
+	end))
 
-	net.up_icon:buttons (awful.button ({}, 1, function()
-		net.up.mode = 1 - net.up.mode
-		vicious.force ({ net.up })
-		end)
-	)
-	net.down_icon:buttons (awful.button ({}, 1, function()
-		net.down.mode = 1 - net.down.mode
-		vicious.force ({ net.down })
-		end)
-	)
-	net.up:buttons (awful.button ({}, 1, function()
-		netup.mode = 1 - netup.mode
-		vicious.force ({ net.up })
-		end)
-	)
-	net.down:buttons (awful.button ({}, 1, function()
-		net.down.mode = 1 - net.down.mode
-		vicious.force ({ net.down })
-		end)
-	)
-
-	return net
+	return self
 end
+
